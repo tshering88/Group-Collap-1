1ccf0c25bc73a9af04e1618de0411a7a5494ff38
nima894 <nimani17403950>
2025-04-28 04:03:11 -0600

    Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import './index.css';
+
+
+import Home from './pages/Home/home';
+import Footer from './pages/Footer/footer';
+import NavBar from './pages/navbar/NavBar';
+
+
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+    <NavBar/>
+    <Home/>
+    
+    
+    <Footer/>
+    </>
   )
 }
 
---
+++ src/pages/learning/Learning.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Learning = () => {
+  return (
+    <ul>
+      <li>Full-Stack JavaScript Development</li>
+      <li>RESTful API Development</li>
+      <li>Database Design & Integration</li>
+      <li>Authentication & Authorization</li>
+      <li>Deployment & DevOps Basics</li>
+    </ul>
+  );
+};
+
+export default Learning;
