5dfe4dd6187d19db02abfd598ee1e46a8245800b
tshering88 <dechentsheringwangdra@gmail.com>
2025-04-29 09:37:49 +0600

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -4,25 +4,11 @@
 import React from 'react';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
-
-
-import React from 'react'
-
-
-
-
 import Home from './pages/Home/home';
 import Footer from './pages/Footer/footer';
 import NavBar from './pages/navbar/NavBar';
-
-import Contacts from './components/Contact/contact'
-import ContactForm from './components/ContactForm'
 import Head from './components/Head'
 import ContactPage from './pages/ContactPage'
-
-
-
-
 import ServicesPage from './pages/Service Page'
 import About from './pages/about us/About';
   
